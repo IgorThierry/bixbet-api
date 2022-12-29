6c7c838e514adc6e274af127bafbe484a958bc42
Igor Thierry <igorthierry15@gmail.com>
2022-12-29 20:04:21 -0300

    feat: first routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import express from 'express';
 
+import swaggerUI from 'swagger-ui-express';
+
+import swaggerFile from './swagger.json';
+
 const app = express();
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
 
 app.listen(3333, () => {
   console.log('🚀 server started on port 3333');
