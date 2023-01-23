6bf218e7ca52bec0421cac44cda1bb7e2f54e6ba
Igor Thierry <igorthierry15@gmail.com>
2023-01-23 16:43:56 -0300

    feat: middleware de autenticação

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { authConfig } from '@config/auth';
+
+import { AppError } from '@shared/errors/AppError';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub: user_id } = decoded as ITokenPayload;
+
+    request.user = {
+      id: user_id,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
